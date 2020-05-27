40ea3c2f646896237b59d25e6ddb02f5840a17a7
林文鑫 <newcloud@yeah.net>
2020-05-27 14:32:49 +0800

    初始化

---
+++ robochat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  robochat
+//
+//  Created by 王庆 on 2020/5/26.
+//  Copyright © 2020 robochat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
