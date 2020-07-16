8a433df144d99ff9b23e8680bbf9718e52c9e282
yangting <yangting@yangtingdeMacBook-Pro.local>
2020-07-16 11:16:16 +0800

    feat: chatView keyboard change

--- robochat/ChatView.swift
+++ robochat/ChatView.swift
@@ -17,82 +17,85 @@ struct ChatView: View {
     private let didShow = NotificationCenter.default.publisher(for: UIApplication.keyboardWillShowNotification)
     private let willHide = NotificationCenter.default.publisher(for: UIApplication.keyboardWillHideNotification)
     var body: some View {
-        VStack{
-            Spacer()
-            HStack {
-                Button(action: {
-                    self.micType = !self.micType
+        GeometryReader {geometry in
+            VStack{
+                Spacer()
+                HStack {
+                    Button(action: {
+                        self.micType = !self.micType
+                        if(self.micType) {
+                            self.cusKeyboradType = false
+                        }
+                    }){
+                        Image(systemName: self.micType ? "calendar.circle" : "mic.circle")
+                            .font(.system(size: 25))
+                            .foregroundColor(.primary)
+                    }
                     if(self.micType) {
-                        self.cusKeyboradType = false
+                        HStack {
+                            Spacer()
+                            Text("按住 说话")
+                            Spacer()
+                        }.frame(height: 40)
+                            .background(Color("chat_send_text_background"))
+                            .padding(.horizontal, 8)
+                    } else {
+                        TextField("", text: self.$msg)
+                            .frame(height: 40)
+                            .background(Color("chat_send_text_background"))
                     }
-                }){
-                    Image(systemName: micType ? "calendar.circle" : "mic.circle")
-                    .font(.system(size: 25))
-                    .foregroundColor(.primary)
-                }
-                if(self.micType) {
-                    HStack {
-                        Spacer()
-                        Text("按住 说话")
-                        Spacer()
-                    }.frame(height: 40)
-                        .background(Color("chat_send_text_background"))
-                        .padding(.horizontal, 8)
-                } else {
-                    TextField("", text: $msg)
-                        .frame(height: 40)
-                        .background(Color("chat_send_text_background"))
-                }
-                Button(action: {
-                    self.cusKeyboradType = !self.cusKeyboradType
-                    if(self.cusKeyboradType) {
-                        self.micType = false
+                    Button(action: {
+                        self.cusKeyboradType = !self.cusKeyboradType
+                        if(self.cusKeyboradType) {
+                            self.micType = false
+                        }
+                    }){
+                        Image(systemName: self.cusKeyboradType ? "calendar.circle" : "smiley")
+                            .font(.system(size: 25))
+                            .foregroundColor(.primary)
                     }
-                }){
-                    Image(systemName: cusKeyboradType ? "calendar.circle" : "smiley")
-                    .font(.system(size: 25))
-                    .foregroundColor(.primary)
+                    Image(systemName: "plus.circle")
+                        .font(.system(size: 25))
+                        .foregroundColor(.primary)
+                }.frame(height: 58)
+                    .padding(.horizontal)
+                    .background(Color("chat_send_background"))
+            }.navigationBarTitle("智能助手", displayMode: .inline)
+                .navigationBarItems(
+                    leading: Button(action: self.toLogin){
+                        Image(systemName: "chevron.left")
+                            .font(.system(size: 20))
+                            .foregroundColor(.primary)
+                    },
+                    trailing: Button(action: self.toLogin){
+                        Image(systemName: "ellipsis")
+                            .font(.system(size: 20))
+                            .foregroundColor(.primary)
+                    }
+                )
+                .padding(.bottom, self.bottom)
+                .onReceive(self.didShow){note in
+                    withAnimation{
+                        self.bottomChange(note, geometry: geometry)
+                    }
                 }
-                Image(systemName: "plus.circle")
-                .font(.system(size: 25))
-                .foregroundColor(.primary)
-            }.frame(height: 58)
-                .padding(.horizontal)
-                .background(Color("chat_send_background"))
-        }.navigationBarTitle("智能助手", displayMode: .inline)
-            .navigationBarItems(
-                leading: Button(action: toLogin){
-                    Image(systemName: "chevron.left")
-                    .font(.system(size: 20))
-                    .foregroundColor(.primary)
-                },
-                trailing: Button(action: toLogin){
-                    Image(systemName: "ellipsis")
-                    .font(.system(size: 20))
-                    .foregroundColor(.primary)
+                .onReceive(self.willHide){note in
+                    self.bottomChange(note, geometry: geometry)
                 }
-        )
-            .padding(.bottom, self.bottom)
-            .onReceive(didShow){note in
-                withAnimation{
-                    self.bottomChange(note)
-                }
-            }
-            .onReceive(willHide){note in
-                self.bottomChange(note)
-            }
+        }
     }
     private func toLogin(){
         withAnimation{
             appData.showLogin = true
         }
     }
-    private func bottomChange(_ notification: Notification) {
+    private func bottomChange(_ notification: Notification, geometry: GeometryProxy) {
+        let space = geometry.safeAreaInsets.bottom
         let userInfo = notification.userInfo;
         let keyboardBeginFrame = userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? CGRect
         let keyboardEndFrame = userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
         let visible = keyboardBeginFrame?.minY ?? 0 > keyboardEndFrame?.minY ?? 0
-        self.bottom = visible ? keyboardEndFrame?.height ?? 0 : 0
+        self.bottom = visible ? (keyboardEndFrame?.height ?? 0) - space : 0
     }
 }
 struct ChatView_Previews: PreviewProvider {
